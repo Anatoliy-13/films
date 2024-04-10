f4eff17437012affc2ef605069ef84d29f3aa220
Anatoliy-13 <arhangeltm13@gmail.com>
2024-04-10 16:57:05 +0200

    feat: add scss

--- my-app/src/featers/Movies/MovieCard.tsx
+++ my-app/src/featers/Movies/MovieCard.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import styles from "./MovieCard.module.scss";
 
 
 interface MovieCardProps {
@@ -10,10 +11,15 @@ interface MovieCardProps {
 
 export function MovieCard({ id, title, overviwe, popularity }: MovieCardProps) {
     return (
-        <div className="movies-card">
-            <Link to={`/movies/${id}`}>{title}</Link>
-            <div className="movies-card-overviwe">{overviwe}</div>
-            <div className="movies-card-popularity">{popularity}</div>
+        <div className={styles.card}>
+            <img className={styles.thumbnail} src="/movie-thumb.png" alt="Movie thumbmail" />
+            <div className={styles.content}>
+                <div>
+                    <Link to={`/movies/${id}`}>{title}</Link>
+                </div>
+                <div className="movies-card-overviwe">{overviwe}</div>
+                <div className="movies-card-popularity">{popularity}</div>
+            </div>
         </div>
     )
 }
