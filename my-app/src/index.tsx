14918f3045c9235cb36c11ebdd9e531b321116f5
Anatoliy-13 <arhangeltm13@gmail.com>
2024-04-10 10:26:58 +0200

    feat: add react router

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { About } from './featers/About/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+    path: "/About",
+    element: <About />,
+  },
+    ]
+  },
+  
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
